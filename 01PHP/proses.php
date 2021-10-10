66962be39f3c068f54fa3eb93001a21f11e14faf
Rizki666 <70094809+Rizki666@users.noreply.github.com>
2021-10-10 23:25:52 +0700

    Add files via upload

---
+++ 01PHP/proses.php
@@ -0,0 +1,5 @@
+<?php
+    $nama = $_POST['Nama'];
+    $alamat = $_POST['Alamat'];
+    echo "nama". $nama . "<br/>" . "Alamat:" . $alamat;
+?>
---
+++ 01PHP/session2.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+echo "Nama adalah" .$_SESSION['ahmad'];
+echo "<br/><a href='sesion1.php'>kembali ke halaman pertama<a/>";
+
+?>
